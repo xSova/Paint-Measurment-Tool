6f90ebf27a4c2e7ebde36617ee3d9dfc3c360d92
Bryce Schultz <bryceschultz2000@yahoo.com>
2023-03-29 22:17:06 -0500

    Added Main Files
    added in the files

---
+++ src/Paint1.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Paint1 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        boolean askForInput = false;
+        double tempValue = 0.0;
+        final double squareFeetPerGallons = 350.0;
+        
+        // Implement a do-while loop to ensure input is valid
+        // Prompt user to input wall's height
+        do {
+        	System.out.println("Enter wall height: ");
+        	try {
+        		wallHeight = scnr.nextInt();
+        		askForInput = false;
+        		if (wallHeight <= 0) {
+        			throw new Exception();
+        		} 
+        	} catch(Exception e) {
+    			scnr.nextLine();
+        		askForInput = true;
+    		}
+        	
+            } while(askForInput);
+        //System.out.println("Wall height collected: " + wallHeight);
+        // Implement a do-while loop to ensure input is valid
+        // Prompt user to input wall's width
+	    do {
+	    	System.out.println("Enter wall width (feet): ");
+	    	try {
+        		wallWidth = scnr.nextInt();
+        		askForInput = false;
+        		if (wallWidth <= 0) {
+        			throw new Exception();
+        		}
+        	} catch(Exception e) {
+        		scnr.nextLine();
+        		askForInput = true;
+        		
+        	}
+	    	} while(askForInput);
+        //System.out.println("Wall width collected: " + wallWidth);
+        // Calculate and output wall area
+        wallArea = (double) wallHeight * (double) wallWidth;
+        System.out.println("Wall area: " + wallArea +  " square feet");
+
+        // Calculate and output the amount of paint (in gallons) needed to paint the wall
+        gallonsPaintNeeded = wallArea / squareFeetPerGallons;
+        System.out.printf("Paint needed: " + gallonsPaintNeeded + " gallons", "%.2f\n");
+
+    }
+}
