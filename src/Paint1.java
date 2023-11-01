bd083272342607e44d2ebe572582a6cac16f985f
Bryce Schultz <25107568+xSova@users.noreply.github.com>
2023-10-31 19:38:34 -0500

    Small adjustments to fix small issues.

--- src/Paint1.java
+++ src/Paint1.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class Paint1 {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         double wallHeight = 0.0;
         double wallWidth = 0.0;
-        double wallArea = 0.0;
-        double gallonsPaintNeeded = 0.0;
-        boolean askForInput = false;
-        double tempValue = 0.0;
-        final double squareFeetPerGallons = 350.0;
+        double wallArea;
+        double gallonsPaintNeeded;
+        boolean askForInput;
+		final double squareFeetPerGallons = 350.0;
         
         // Implement a do-while loop to ensure input is valid
         // Prompt user to input wall's height
@@ -48,7 +46,7 @@ public class Paint1 {
 	    	} while(askForInput);
         //System.out.println("Wall width collected: " + wallWidth);
         // Calculate and output wall area
-        wallArea = (double) wallHeight * (double) wallWidth;
+        wallArea = wallHeight * wallWidth;
         System.out.println("Wall area: " + wallArea +  " square feet");
 
         // Calculate and output the amount of paint (in gallons) needed to paint the wall
